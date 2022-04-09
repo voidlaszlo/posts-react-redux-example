2df0c945de628577f79cacd39260b693b5544a9b
voidlaszlo <voidlaszlo@gmail.com>
2022-04-09 22:58:49 +0200

    renamed poorly named variable in Posts.tsx

--- src/pages/Posts.tsx
+++ src/pages/Posts.tsx
@@ -133,35 +133,30 @@ const Posts = () => {
       <div className="mt-4">
         <h1 className="sr-only">Recent posts</h1>
         <ul className="space-y-4">
-          {postsToShow.map((question) => (
+          {postsToShow.map((post) => (
             <li
-              key={question.id}
+              key={post.id}
               className="bg-white px-4 py-6 shadow sm:p-6 sm:rounded-lg"
             >
-              <article aria-labelledby={"question-title-" + question.id}>
+              <article aria-labelledby={"post-title-" + post.id}>
                 <div>
                   <div className="flex space-x-3">
                     <div className="flex-shrink-0">
                       <img
                         className="h-10 w-10 rounded-full"
-                        src={question.author.imageUrl}
+                        src={post.author.imageUrl}
                         alt=""
                       />
                     </div>
                     <div className="min-w-0 flex-1">
                       <p className="text-sm font-medium text-gray-900">
-                        <a
-                          href={question.author.href}
-                          className="hover:underline"
-                        >
-                          {question.author.name}
+                        <a href={post.author.href} className="hover:underline">
+                          {post.author.name}
                         </a>
                       </p>
                       <p className="text-sm text-gray-500">
-                        <a href={question.href} className="hover:underline">
-                          <time dateTime={question.datetime}>
-                            {question.date}
-                          </time>
+                        <a href={post.href} className="hover:underline">
+                          <time dateTime={post.datetime}>{post.date}</time>
                         </a>
                       </p>
                     </div>
@@ -255,15 +250,15 @@ const Posts = () => {
                     </div>
                   </div>
                   <h2
-                    id={"question-title-" + question.id}
+                    id={"post-title-" + post.id}
                     className="mt-4 text-base font-medium text-gray-900"
                   >
-                    {question.title}
+                    {post.title}
                   </h2>
                 </div>
                 <div
                   className="mt-2 text-sm text-gray-700 space-y-4"
-                  dangerouslySetInnerHTML={{ __html: question.body }}
+                  dangerouslySetInnerHTML={{ __html: post.body }}
                 />
                 <div className="mt-6 flex justify-between space-x-8">
                   <div className="flex space-x-6">
@@ -274,7 +269,7 @@ const Posts = () => {
                       >
                         <ThumbUpIcon className="h-5 w-5" aria-hidden="true" />
                         <span className="font-medium text-gray-900">
-                          {question.likes}
+                          {post.likes}
                         </span>
                         <span className="sr-only">likes</span>
                       </button>
@@ -286,7 +281,7 @@ const Posts = () => {
                       >
                         <ChatAltIcon className="h-5 w-5" aria-hidden="true" />
                         <span className="font-medium text-gray-900">
-                          {question.replies}
+                          {post.replies}
                         </span>
                         <span className="sr-only">replies</span>
                       </button>
@@ -298,7 +293,7 @@ const Posts = () => {
                       >
                         <EyeIcon className="h-5 w-5" aria-hidden="true" />
                         <span className="font-medium text-gray-900">
-                          {question.views}
+                          {post.views}
                         </span>
                         <span className="sr-only">views</span>
                       </button>
